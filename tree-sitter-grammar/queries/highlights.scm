95ccad4e7698d45be3dc26b5f1645a64ce6fd8ae
Thibault Cheneviere <thibault.chene23@gmail.com>
2025-08-05 12:21:46 +0300

    feat: added a tree-sitter grammar and queries to highlight hsl code

---
+++ tree-sitter-grammar/queries/highlights.scm
@@ -0,0 +1,43 @@
+; Identifier
+
+(identifier) @variable
+
+; Function calls
+
+(function_call
+  name: (identifier) @function)
+
+; Function declaration
+
+(function_definition
+  name: (identifier) @function)
+
+; Literals
+
+(integer) @constant.builtin
+(bool) @constant.builtin
+
+(char) @string
+(string) @string
+
+; Comments
+
+(line_comment) @comment
+
+; Keywords
+
+[
+  "Hypersignal"
+  "Jamsignal"
+  "A long time ago in a"
+  "far, far away..."
+  "May the force be with you."
+  "I am a big deal in the resistance."
+  "The force is strong with this one."
+  "That's one hell of a pilot."
+  "I am your father."
+  "Judge me by my size, do you ?"
+  "Execute order"
+  "Order executed"
+  "Starfield"
+] @keyword
